2eea917dcc7c0f4443d1ce2fe99ea6ad7dfde0dd
nrb2002 <tshibasubaron@yahoo.fr>
2024-11-23 04:09:42 +0100

    Update assignment2.sql

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -27,4 +27,4 @@ SELECT public.inventory.inv_make, public.inventory.inv_model, public.classificat
 Query 6 -- Update all records in the inventory table to add "/vehicles" to the middle of the file path in the inv_image and inv_thumbnail columns using a single query. This reference may prove helpful - https://www.postgresqltutorial.com/postgresql-string-functions/postgresql-replace/. When done the path for both inv_image and inv_thumbnail should resemble this example: /images/vehicles/a-car-name.jpg
 */
 UPDATE public.inventory
-	SET inv_image = REPLACE()inv_image, '/images', '/images/vehicles';
+	SET inv_image = REPLACE(inv_image, '/images', '/images/vehicles');
