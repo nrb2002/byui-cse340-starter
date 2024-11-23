6b927fb9f6578ab681fb5ea5bf509bfbab10cd5c
nrb2002 <tshibasubaron@yahoo.fr>
2024-11-23 19:38:55 +0100

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,36 +1,42 @@
-const{Pool} = require("pg"); //import the Pool functionality from the PostgreSQL package
-require("dotenv").config() //import the "dotenv" package which allows the sensitive information about the database
+const { Pool } = require("pg") //Import the Pool functionality from the pg package
+require("dotenv").config() //Import dotenv package that stores sensitive info about the database location and connection
 /* ***************
  * Connection Pool
  * SSL Object needed for local testing of app
  * But will cause problems in production environment
  * If - else will make determination which to use
  * *************** */
-let pool //creates a local pool variable to hold the functionality of the "Pool" connection.
-if(process.env.NODE_ENV == "development"){
+let pool //Create a local pool variable
+
+//Check to see if the code exists in a development environment
+if (process.env.NODE_ENV == "development") {
+    //create a new pool instance from the imported Pool class
     pool = new Pool({
-        connectionString: process.env.DATABASE_URL, 
+        //Set the connection string as stored in the .env file
+        connectionString: process.env.DATABASE_URL,
         ssl: {
-            rejectUnauthorized: false, //Do not reject connection when connecting with a remote database
-        },    
+            //prevent server from rejecting remote connection
+        rejectUnauthorized: false,
+        },
     })
+
     // Added for troubleshooting queries
-    // during development
+    // During development
     module.exports = {
-        async query(text, params){
-            try{
+        async query(text, params) {
+            try {
                 const res = await pool.query(text, params)
-                console.log("executer query", {text})
+                console.log("executed query", { text })
                 return res
-            } catch(error){
-                console.error('error in query', {text})
+            } catch (error) {
+                console.error("error in query", { text })
                 throw error
-            } 
+            }
         },
     }
-} else{
+} else {
     pool = new Pool({
-        connectionString: process.env.DATABASE_URL, //indicates the value of the connection string will be found in an environment variable 
-    })    
+        connectionString: process.env.DATABASE_URL,
+    })
     module.exports = pool
 }
