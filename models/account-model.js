e78e9376214a5b88c84589399d3819faea61d026
nrb2002 <tshibasubaron@yahoo.fr>
2024-12-14 05:32:00 +0100

    Update account-model.js

--- models/account-model.js
+++ models/account-model.js
@@ -23,14 +23,14 @@ async function registerAccount(account_firstname, account_lastname, account_emai
 Hopefully, this function looks familiar. 
 It queries the database to see if a record exists with the same email that is being submitted. It returns the count of rows found. Anything greater than zero means the email already exists in the database.
 */
-// async function checkExistingEmail(account_email){
-//     try {
-//       const sql = "SELECT * FROM account WHERE account_email = $1"
-//       const email = await pool.query(sql, [account_email])
-//       return email.rowCount
-//     } catch (error) {
-//       return error.message
-//     }
-//   }
+async function checkExistingEmail(account_email){
+    try {
+      const sql = "SELECT * FROM account WHERE account_email = $1"
+      const email = await pool.query(sql, [account_email])
+      return email.rowCount
+    } catch (error) {
+      return error.message
+    }
+  }
 
-  module.exports = { registerAccount }
+module.exports = { registerAccount, checkExistingEmail }
